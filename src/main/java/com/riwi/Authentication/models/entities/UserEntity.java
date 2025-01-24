20e8cdee57366ddfe34a6ba19162de1f3de8ff77
rafez963 <rafaelmeza963san@gmail.com>
2025-01-24 18:07:44 -0500

    create file userEntity

---
+++ src/main/java/com/riwi/Authentication/models/entities/UserEntity.java
@@ -0,0 +1,36 @@
+package com.riwi.Authentication.models.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 150)
+    private String name;
+
+    @Column(nullable = false, length = 150)
+    private String lastname;
+
+    @Column(nullable = false, length = 255)
+    private String password;
+
+    @Column(nullable = false, length = 255)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 15)
+    private Role role;
+}
