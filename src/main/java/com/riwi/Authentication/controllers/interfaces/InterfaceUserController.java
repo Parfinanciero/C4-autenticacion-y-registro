a8c9bb641045d632e2199e400da0a790ce077028
rafez963 <rafaelmeza963san@gmail.com>
2025-01-27 17:15:53 -0500

    fix(interface): corregir errores en feature/file-interface

--- src/main/java/com/riwi/Authentication/controllers/interfaces/InterfaceUserController.java
+++ src/main/java/com/riwi/Authentication/controllers/interfaces/InterfaceUserController.java
@@ -1,26 +1,19 @@
 package com.riwi.Authentication.controllers.interfaces;
 
+import com.riwi.Authentication.controllers.generics.Byid;
 import com.riwi.Authentication.dtos.requests.UserRequest;
 import com.riwi.Authentication.dtos.response.UserResponse;
+import com.riwi.Authentication.servicies.generics.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-public interface InterfaceUserController {
-
-    @PostMapping
-    ResponseEntity<UserResponse> createUser(@RequestBody UserRequest userRequest);
-
-    @GetMapping("/{id}")
-    ResponseEntity<UserResponse> getUserById(@PathVariable Long id);
-
-    @GetMapping
-    ResponseEntity<List<UserResponse>> getAllUsers();
-
-    @PutMapping("/{id}")
-    ResponseEntity<UserResponse> updateUser(@PathVariable Long id, @RequestBody UserRequest userRequestDTO);
-
-    @DeleteMapping("/{id}")
-    ResponseEntity<Void> deleteUser(@PathVariable Long id);
+public interface InterfaceUserController extends
+        Byid<Long, UserResponse>,
+        Create<UserRequest, Long>,
+        Delete<Long>,
+        ReadAll<UserResponse>,
+        Put<UserRequest, Long>,
+        Path<UserRequest, Long>{
 }
