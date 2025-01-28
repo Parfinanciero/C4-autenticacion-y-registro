c2a13e4b9e1a140f9c6205ed63737f8591d17767
rafez963 <rafaelmeza963san@gmail.com>
2025-01-27 19:33:36 -0500

    fix(controller): resolver conflictos y actualizar UserController

--- src/main/java/com/riwi/Authentication/controllers/impl/UserController.java
+++ src/main/java/com/riwi/Authentication/controllers/impl/UserController.java
@@ -7,7 +7,6 @@ import com.riwi.Authentication.servicies.interfaces.IUService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
